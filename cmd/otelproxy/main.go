d3d1688ee83f870e96e6b63eadc66eedddc0fb74
tdakkota <tanc13@yandex.ru>
2023-06-14 21:39:10 +0300

    feat(otelproxy): add setup for all datasources

--- cmd/otelproxy/main.go
+++ cmd/otelproxy/main.go
@@ -1,9 +1,11 @@
+// Command otelproxy is a Grafana datasource proxy.
 package main
 
 import (
 	"context"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/go-faster/errors"
@@ -11,57 +13,250 @@ import (
 	"go.uber.org/zap"
 	"golang.org/x/sync/errgroup"
 
+	"github.com/go-faster/oteldb/internal/lokiapi"
+	"github.com/go-faster/oteldb/internal/lokiproxy"
 	"github.com/go-faster/oteldb/internal/promapi"
 	"github.com/go-faster/oteldb/internal/promproxy"
+	"github.com/go-faster/oteldb/internal/pyroproxy"
+	"github.com/go-faster/oteldb/internal/pyroscopeapi"
+	"github.com/go-faster/oteldb/internal/tempoapi"
+	"github.com/go-faster/oteldb/internal/tempoproxy"
 )
+
+type service struct {
+	addr    string
+	name    string
+	handler http.Handler
+}
+
+func (s service) Run(ctx context.Context, lg *zap.Logger) error {
+	httpServer := &http.Server{
+		Addr:              s.addr,
+		Handler:           s.handler,
+		ReadHeaderTimeout: 15 * time.Second,
+	}
+	lg.Info("Starting HTTP server",
+		zap.String("addr", s.addr),
+	)
+	parentCtx := ctx
+	g, ctx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		<-ctx.Done()
+
+		lg.Info("Shutting down")
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		return httpServer.Shutdown(ctx)
+	})
+	g.Go(func() error {
+		if err := httpServer.ListenAndServe(); err != nil {
+			if errors.Is(err, http.ErrServerClosed) && parentCtx.Err() != nil {
+				lg.Info("HTTP server closed gracefully")
+				return nil
+			}
+			return errors.Wrap(err, "http server")
+		}
+		return nil
+	})
+	return g.Wait()
+}
+
+type services struct {
+	ports map[string]service
+}
+
+func (s *services) addService(addr string, srv service) error {
+	if s.ports == nil {
+		s.ports = map[string]service{}
+	}
+	if existing, ok := s.ports[addr]; ok {
+		return errors.Errorf("service conflict: %q and %q on %q", existing.name, srv.name, addr)
+	}
+
+	s.ports[addr] = srv
+	return nil
+}
+
+func (s *services) Prometheus(m *app.Metrics) error {
+	const (
+		prefix      = "PROMETHEUS"
+		defaultPort = ":9090"
+	)
+
+	upstreamURL := os.Getenv(prefix + "_URL")
+	if upstreamURL == "" {
+		return nil
+	}
+	client, err := promapi.NewClient(upstreamURL,
+		promapi.WithTracerProvider(m.TracerProvider()),
+		promapi.WithMeterProvider(m.MeterProvider()),
+	)
+	if err != nil {
+		return errors.Wrap(err, "create client")
+	}
+
+	server, err := promapi.NewServer(
+		promproxy.NewServer(client),
+		promapi.WithTracerProvider(m.TracerProvider()),
+		promapi.WithMeterProvider(m.MeterProvider()),
+	)
+	if err != nil {
+		return errors.Wrap(err, "create server")
+	}
+	addr := os.Getenv(prefix + "_HTTP_ADDR")
+	if addr == "" {
+		addr = defaultPort
+	}
+
+	return s.addService(addr, service{
+		addr:    addr,
+		name:    strings.ToLower(prefix),
+		handler: server,
+	})
+}
+
+func (s *services) Loki(m *app.Metrics) error {
+	const (
+		prefix      = "LOKI"
+		defaultPort = ":3100"
+	)
+
+	upstreamURL := os.Getenv(prefix + "_URL")
+	if upstreamURL == "" {
+		return nil
+	}
+	client, err := lokiapi.NewClient(upstreamURL,
+		lokiapi.WithTracerProvider(m.TracerProvider()),
+		lokiapi.WithMeterProvider(m.MeterProvider()),
+	)
+	if err != nil {
+		return errors.Wrap(err, "create client")
+	}
+
+	server, err := lokiapi.NewServer(
+		lokiproxy.NewServer(client),
+		lokiapi.WithTracerProvider(m.TracerProvider()),
+		lokiapi.WithMeterProvider(m.MeterProvider()),
+	)
+	if err != nil {
+		return errors.Wrap(err, "create server")
+	}
+	addr := os.Getenv(prefix + "_HTTP_ADDR")
+	if addr == "" {
+		addr = defaultPort
+	}
+
+	return s.addService(addr, service{
+		addr:    addr,
+		name:    strings.ToLower(prefix),
+		handler: server,
+	})
+}
+
+func (s *services) Pyroscope(m *app.Metrics) error {
+	const (
+		prefix      = "PYROSCOPE"
+		defaultPort = ":4040"
+	)
+
+	upstreamURL := os.Getenv(prefix + "_URL")
+	if upstreamURL == "" {
+		return nil
+	}
+	client, err := pyroscopeapi.NewClient(upstreamURL,
+		pyroscopeapi.WithTracerProvider(m.TracerProvider()),
+		pyroscopeapi.WithMeterProvider(m.MeterProvider()),
+	)
+	if err != nil {
+		return errors.Wrap(err, "create client")
+	}
+
+	server, err := pyroscopeapi.NewServer(
+		pyroproxy.NewServer(client),
+		pyroscopeapi.WithTracerProvider(m.TracerProvider()),
+		pyroscopeapi.WithMeterProvider(m.MeterProvider()),
+	)
+	if err != nil {
+		return errors.Wrap(err, "create server")
+	}
+	addr := os.Getenv(prefix + "_HTTP_ADDR")
+	if addr == "" {
+		addr = defaultPort
+	}
+
+	return s.addService(addr, service{
+		addr:    addr,
+		name:    strings.ToLower(prefix),
+		handler: server,
+	})
+}
+
+func (s *services) Tempo(m *app.Metrics) error {
+	const (
+		prefix      = "TEMPO"
+		defaultPort = ":3200"
+	)
+
+	upstreamURL := os.Getenv(prefix + "_URL")
+	if upstreamURL == "" {
+		return nil
+	}
+	client, err := tempoapi.NewClient(upstreamURL,
+		tempoapi.WithTracerProvider(m.TracerProvider()),
+		tempoapi.WithMeterProvider(m.MeterProvider()),
+	)
+	if err != nil {
+		return errors.Wrap(err, "create client")
+	}
+
+	server, err := tempoapi.NewServer(
+		tempoproxy.NewServer(client),
+		tempoapi.WithTracerProvider(m.TracerProvider()),
+		tempoapi.WithMeterProvider(m.MeterProvider()),
+	)
+	if err != nil {
+		return errors.Wrap(err, "create server")
+	}
+	addr := os.Getenv(prefix + "_HTTP_ADDR")
+	if addr == "" {
+		addr = defaultPort
+	}
+
+	return s.addService(addr, service{
+		addr:    addr,
+		name:    strings.ToLower(prefix),
+		handler: server,
+	})
+}
 
 func main() {
 	app.Run(func(ctx context.Context, lg *zap.Logger, m *app.Metrics) error {
-		promURL := os.Getenv("PROMETHEUS_URL")
-		promClient, err := promapi.NewClient(promURL,
-			promapi.WithTracerProvider(m.TracerProvider()),
-			promapi.WithMeterProvider(m.MeterProvider()),
-		)
-		if err != nil {
-			return errors.Wrap(err, "create prometheus client")
+		s := services{}
+		if err := s.Prometheus(m); err != nil {
+			return errors.Wrapf(err, "setup Prometheus proxy")
 		}
-		promServer, err := promapi.NewServer(promproxy.NewServer(promClient),
-			promapi.WithTracerProvider(m.TracerProvider()),
-			promapi.WithMeterProvider(m.MeterProvider()),
-		)
-		if err != nil {
-			return errors.Wrap(err, "create prometheus server")
+		if err := s.Loki(m); err != nil {
+			return errors.Wrapf(err, "setup Loki proxy")
 		}
-		addr := os.Getenv("HTTP_ADDR")
-		if addr == "" {
-			// Default Prometheus port.
-			addr = ":9090"
+		if err := s.Pyroscope(m); err != nil {
+			return errors.Wrapf(err, "setup Pyroscope proxy")
 		}
-		httpServer := &http.Server{
-			Addr:              addr,
-			Handler:           promServer,
-			ReadHeaderTimeout: 15 * time.Second,
+		if err := s.Tempo(m); err != nil {
+			return errors.Wrapf(err, "setup Tempo proxy")
 		}
-		lg.Info("Starting HTTP server", zap.String("addr", addr))
-		parentCtx := ctx
-		g, ctx := errgroup.WithContext(ctx)
-		g.Go(func() error {
-			<-ctx.Done()
-			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-			defer cancel()
 
-			return httpServer.Shutdown(ctx)
-		})
-		g.Go(func() error {
-			if err := httpServer.ListenAndServe(); err != nil {
-				if errors.Is(err, http.ErrServerClosed) && parentCtx.Err() != nil {
-					lg.Info("HTTP server closed gracefully")
-					return nil
-				}
-				return errors.Wrap(err, "http server")
-			}
-			return nil
-		})
+		if len(s.ports) == 0 {
+			return errors.New("no services to run")
+		}
+		g, ctx := errgroup.WithContext(ctx)
+		for _, s := range s.ports {
+			s := s
+			lg := lg.Named(s.name)
+			g.Go(func() error {
+				return s.Run(ctx, lg)
+			})
+		}
 		return g.Wait()
 	})
 }
