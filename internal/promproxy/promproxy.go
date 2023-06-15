acf35ec2e3f93dcde3b87b66e6bca0c8054c5a89
tdakkota <tanc13@yandex.ru>
2023-06-15 17:14:12 +0300

    chore(promproxy): update proxy due to spec changes

--- internal/promproxy/promproxy.go
+++ internal/promproxy/promproxy.go
@@ -24,13 +24,65 @@ type Server struct {
 	api *promapi.Client
 }
 
+// GetLabelValues implements getLabelValues operation.
+// GET /api/v1/label/{label}/values
+func (s *Server) GetLabelValues(ctx context.Context, params promapi.GetLabelValuesParams) (*promapi.LabelValuesResponse, error) {
+	return s.api.GetLabelValues(ctx, params)
+}
+
+// GetLabels implements getLabels operation.
+//
+// GET /api/v1/labels
+func (s *Server) GetLabels(ctx context.Context, params promapi.GetLabelsParams) (*promapi.LabelsResponse, error) {
+	return s.api.GetLabels(ctx, params)
+}
+
+// GetMetadata implements getMetadata operation.
+//
+// GET /api/v1/metadata
+func (s *Server) GetMetadata(ctx context.Context, params promapi.GetMetadataParams) (*promapi.MetadataResponse, error) {
+	return s.api.GetMetadata(ctx, params)
+}
+
 // GetQuery implements getQuery operation.
 //
 // Query Prometheus.
 //
 // GET /api/v1/query
-func (s Server) GetQuery(ctx context.Context, params promapi.GetQueryParams) (*promapi.Success, error) {
+func (s *Server) GetQuery(ctx context.Context, params promapi.GetQueryParams) (*promapi.QueryResponse, error) {
 	return s.api.GetQuery(ctx, params)
+}
+
+// GetQueryExemplars implements getQueryExemplars operation.
+//
+// Query Prometheus.
+//
+// GET /api/v1/query_examplars
+func (s *Server) GetQueryExemplars(ctx context.Context, params promapi.GetQueryExemplarsParams) (*promapi.QueryExemplarsResponse, error) {
+	return s.api.GetQueryExemplars(ctx, params)
+}
+
+// GetQueryRange implements getQueryRange operation.
+//
+// Query Prometheus.
+//
+// GET /api/v1/query_range
+func (s *Server) GetQueryRange(ctx context.Context, params promapi.GetQueryRangeParams) (*promapi.QueryResponse, error) {
+	return s.api.GetQueryRange(ctx, params)
+}
+
+// GetRules implements getRules operation.
+//
+// GET /api/v1/rules
+func (s *Server) GetRules(ctx context.Context, params promapi.GetRulesParams) (*promapi.RulesResponse, error) {
+	return s.api.GetRules(ctx, params)
+}
+
+// PostLabels implements postLabels operation.
+//
+// POST /api/v1/labels
+func (s *Server) PostLabels(ctx context.Context) (*promapi.LabelsResponse, error) {
+	return s.api.PostLabels(ctx)
 }
 
 // PostQuery implements postQuery operation.
@@ -38,8 +90,26 @@ func (s Server) GetQuery(ctx context.Context, params promapi.GetQueryParams) (*p
 // Query Prometheus.
 //
 // POST /api/v1/query
-func (s Server) PostQuery(ctx context.Context) (*promapi.Success, error) {
+func (s *Server) PostQuery(ctx context.Context) (*promapi.QueryResponse, error) {
 	return s.api.PostQuery(ctx)
+}
+
+// PostQueryExemplars implements postQueryExemplars operation.
+//
+// Query Prometheus.
+//
+// POST /api/v1/query_examplars
+func (s *Server) PostQueryExemplars(ctx context.Context) (*promapi.QueryExemplarsResponse, error) {
+	return s.api.PostQueryExemplars(ctx)
+}
+
+// PostQueryRange implements postQueryRange operation.
+//
+// Query Prometheus.
+//
+// POST /api/v1/query_range
+func (s *Server) PostQueryRange(ctx context.Context) (*promapi.QueryResponse, error) {
+	return s.api.PostQueryRange(ctx)
 }
 
 // NewError creates *FailStatusCode from error returned by handler.
