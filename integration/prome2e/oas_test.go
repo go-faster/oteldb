493c2e617ab71d86ab52b10cae20d927d41b1744
tdakkota <tanc13@yandex.ru>
2023-06-16 21:07:34 +0300

    test(prome2e): add tests for all operations

--- integration/prome2e/oas_test.go
+++ integration/prome2e/oas_test.go
@@ -45,7 +45,7 @@ func TestPrometheusOAS(t *testing.T) {
 	// Provide config to prometheus testcontainer.
 	dir := t.TempDir()
 	configPath := filepath.Join(dir, "prometheus.yml")
-	require.NoError(t, os.WriteFile(configPath, prometheusConfig, 0644))
+	require.NoError(t, os.WriteFile(configPath, prometheusConfig, 0o644))
 
 	req := testcontainers.ContainerRequest{
 		Name:         "oteldb-e2e-prom",
@@ -88,32 +88,106 @@ func TestPrometheusOAS(t *testing.T) {
 	api, err := promapi.NewClient(u.String())
 	require.NoError(t, err, "api client")
 
-	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
-	defer cancel()
-	bo := backoff.WithContext(backoff.NewConstantBackOff(time.Millisecond*100), ctx)
-	v, err := backoff.RetryWithData(func() (*promapi.Vector, error) {
-		res, err := api.GetQuery(ctx, promapi.GetQueryParams{
-			Query: "go_info{}",
-		})
-		if err != nil {
-			return nil, errors.Wrap(err, "api query")
-		}
-		v, ok := res.Data.GetVector()
-		if !ok {
-			return nil, backoff.Permanent(errors.New("not a vector"))
-		}
-		if len(v.Result) == 0 {
-			// Waiting to be scraped.
-			return nil, errors.New("empty vector")
-		}
-		return &v, nil
-	}, bo)
-	require.NoError(t, err, "api query")
-	require.Len(t, v.Result, 1, "should be one result")
-	printJSON(t, v, "query result")
+	{
+		bo := backoff.WithContext(backoff.NewConstantBackOff(time.Millisecond*100), ctx)
+		v, err := backoff.RetryWithData(func() (v promapi.Vector, _ error) {
+			res, err := api.GetQuery(ctx, promapi.GetQueryParams{
+				Query: "go_info{}",
+			})
+			if err != nil {
+				return v, errors.Wrap(err, "api query")
+			}
+			v, ok := res.Data.GetVector()
+			if !ok {
+				return v, backoff.Permanent(errors.New("not a vector"))
+			}
+			if len(v.Result) == 0 {
+				// Waiting to be scraped.
+				return v, errors.New("empty vector")
+			}
+			return v, nil
+		}, bo)
+		require.NoError(t, err, "api query")
+		require.Len(t, v.Result, 1, "should be one result")
+		printJSON(t, &v, "query result")
 
-	vr := v.Result[0]
-	assert.Equal(t, "go_info", vr.Metric["__name__"], "metric name")
-	assert.Greater(t, vr.Value.T, 0.0, "value")
-	assert.NotEmpty(t, vr.Value.V)
+		vr := v.Result[0]
+		assert.Equal(t, "go_info", vr.Metric["__name__"], "metric name")
+		assert.Greater(t, vr.Value.T, 0.0, "value")
+		assert.NotEmpty(t, vr.Value.V)
+	}
+
+	var (
+		now   = time.Now()
+		start = promapi.PrometheusTimestamp(now.Add(-15 * time.Minute).Format(time.RFC3339))
+		end   = promapi.PrometheusTimestamp(now.Add(+15 * time.Minute).Format(time.RFC3339))
+	)
+	t.Run("QueryRange", func(t *testing.T) {
+		res, err := api.GetQueryRange(ctx, promapi.GetQueryRangeParams{
+			Query: "go_info{}",
+			Start: start,
+			End:   end,
+			Step:  "15s",
+		})
+		require.NoError(t, err)
+		m, ok := res.Data.GetMatrix()
+		require.True(t, ok, "not a matrix")
+
+		require.NotEmpty(t, m.Result)
+		mr := m.Result[0]
+		assert.Equal(t, "go_info", mr.Metric["__name__"], "metric name")
+	})
+	t.Run("QueryExemplars", func(t *testing.T) {
+		// By default, Prometheus does not store any exemplars.
+		//
+		// But Grafana may query it, so make sure that request is properly encoded
+		// and we able to parse response.
+		//
+		// See https://prometheus.io/docs/prometheus/latest/feature_flags/#exemplars-storage for more
+		// info about exemplars.
+		res, err := api.GetQueryExemplars(ctx, promapi.GetQueryExemplarsParams{
+			Query: "go_goroutines{}",
+			Start: start,
+			End:   end,
+		})
+		require.NoError(t, err)
+		require.Equal(t, "success", res.Status)
+	})
+	t.Run("Series", func(t *testing.T) {
+		res, err := api.GetSeries(ctx, promapi.GetSeriesParams{
+			Start: start,
+			End:   end,
+			Match: []string{`go_info{job="prometheus"}`},
+		})
+		require.NoError(t, err)
+		require.NotEmpty(t, res.Data)
+		printJSON(t, res.Data, "series")
+
+		m := res.Data[0]
+		assert.Equal(t, "go_info", m["__name__"])
+		assert.Equal(t, "prometheus", m["job"])
+	})
+	t.Run("Labels", func(t *testing.T) {
+		labels, err := api.GetLabels(ctx, promapi.GetLabelsParams{})
+		require.NoError(t, err)
+		require.NotEmpty(t, labels.Data)
+		printJSON(t, labels, "labels")
+
+		label := labels.Data[0]
+		values, err := api.GetLabelValues(ctx, promapi.GetLabelValuesParams{
+			Label: label,
+		})
+		require.NoError(t, err)
+		require.NotEmpty(t, values.Data)
+		printJSON(t, values, "label values")
+	})
+	t.Run("Metadata", func(t *testing.T) {
+		res, err := api.GetMetadata(ctx, promapi.GetMetadataParams{})
+		require.NoError(t, err)
+		require.NotEmpty(t, res.Data)
+		printJSON(t, res.Data, "metadata")
+
+		m := res.Data
+		assert.NotEmpty(t, m["go_info"])
+	})
 }
