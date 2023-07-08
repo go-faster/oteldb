03b2f2538859e161ca8aaaf8d296696146bb2277
tdakkota <tanc13@yandex.ru>
2023-07-08 21:51:38 +0300

    test(logqlengine): add label filter tests

--- internal/logql/logqlengine/label_filter_test.go
+++ internal/logql/logqlengine/label_filter_test.go
@@ -3,6 +3,7 @@ package logqlengine
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/pdata/pcommon"
@@ -10,12 +11,408 @@ import (
 	"github.com/go-faster/oteldb/internal/logql"
 )
 
+func TestDurationLabelFilter(t *testing.T) {
+	tests := []struct {
+		input         map[string]pcommon.Value
+		label         string
+		op            logql.BinOp
+		value         time.Duration
+		wantOk        bool
+		wantFilterErr bool
+	}{
+		// No label.
+		{
+			map[string]pcommon.Value{},
+			`not_exist`,
+			logql.OpEq,
+			10 * time.Second,
+			false,
+			false,
+		},
+
+		// Has match.
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr("10s"),
+			},
+			"exist",
+			logql.OpEq,
+			10 * time.Second,
+			true,
+			false,
+		},
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr("9s"),
+			},
+			"exist",
+			logql.OpNotEq,
+			10 * time.Second,
+			true,
+			false,
+		},
+		// Greater than.
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr("11s"),
+			},
+			"exist",
+			logql.OpGt,
+			10 * time.Second,
+			true,
+			false,
+		},
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr("11s"),
+			},
+			"exist",
+			logql.OpGte,
+			10 * time.Second,
+			true,
+			false,
+		},
+		// Less than.
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr("9s"),
+			},
+			"exist",
+			logql.OpLt,
+			10 * time.Second,
+			true,
+			false,
+		},
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr("9s"),
+			},
+			"exist",
+			logql.OpLte,
+			10 * time.Second,
+			true,
+			false,
+		},
+
+		// Parsing error.
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr(`foo`),
+			},
+			"exist",
+			logql.OpEq,
+			10 * time.Second,
+			false,
+			true,
+		},
+	}
+	for i, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("Test%d", i+1), func(t *testing.T) {
+			set := newLabelSet()
+			set.labels = tt.input
+
+			f, err := buildDurationLabelFilter(&logql.DurationFilter{
+				Label: logql.Label(tt.label),
+				Op:    tt.op,
+				Value: tt.value,
+			})
+			require.NoError(t, err)
+
+			newLine, gotOk := f.Process(0, ``, set)
+			// Ensure that extractor does not change the line.
+			require.Equal(t, ``, newLine)
+
+			if tt.wantFilterErr {
+				_, ok := set.GetError()
+				require.True(t, ok)
+				return
+			}
+			errMsg, ok := set.GetError()
+			require.False(t, ok, "got error: %s", errMsg)
+
+			require.Equal(t, tt.wantOk, gotOk)
+		})
+	}
+}
+
+func TestBytesLabelFilter(t *testing.T) {
+	tests := []struct {
+		input         map[string]pcommon.Value
+		label         string
+		op            logql.BinOp
+		value         uint64
+		wantOk        bool
+		wantFilterErr bool
+	}{
+		// No label.
+		{
+			map[string]pcommon.Value{},
+			`not_exist`,
+			logql.OpEq,
+			10,
+			false,
+			false,
+		},
+
+		// Has match.
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr("10b"),
+			},
+			"exist",
+			logql.OpEq,
+			10,
+			true,
+			false,
+		},
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr("9b"),
+			},
+			"exist",
+			logql.OpNotEq,
+			10,
+			true,
+			false,
+		},
+		// Greater than.
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr("11b"),
+			},
+			"exist",
+			logql.OpGt,
+			10,
+			true,
+			false,
+		},
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr("11b"),
+			},
+			"exist",
+			logql.OpGte,
+			10,
+			true,
+			false,
+		},
+		// Less than.
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr("9b"),
+			},
+			"exist",
+			logql.OpLt,
+			10,
+			true,
+			false,
+		},
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr("9b"),
+			},
+			"exist",
+			logql.OpLte,
+			10,
+			true,
+			false,
+		},
+
+		// Parsing error.
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr(`foo`),
+			},
+			"exist",
+			logql.OpEq,
+			10,
+			false,
+			true,
+		},
+	}
+	for i, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("Test%d", i+1), func(t *testing.T) {
+			set := newLabelSet()
+			set.labels = tt.input
+
+			f, err := buildBytesLabelFilter(&logql.BytesFilter{
+				Label: logql.Label(tt.label),
+				Op:    tt.op,
+				Value: tt.value,
+			})
+			require.NoError(t, err)
+
+			newLine, gotOk := f.Process(0, ``, set)
+			// Ensure that extractor does not change the line.
+			require.Equal(t, ``, newLine)
+
+			if tt.wantFilterErr {
+				_, ok := set.GetError()
+				require.True(t, ok)
+				return
+			}
+			errMsg, ok := set.GetError()
+			require.False(t, ok, "got error: %s", errMsg)
+
+			require.Equal(t, tt.wantOk, gotOk)
+		})
+	}
+}
+
+func TestNumberLabelFilter(t *testing.T) {
+	tests := []struct {
+		input         map[string]pcommon.Value
+		label         string
+		op            logql.BinOp
+		value         float64
+		wantOk        bool
+		wantFilterErr bool
+	}{
+		// No label.
+		{
+			map[string]pcommon.Value{},
+			`not_exist`,
+			logql.OpEq,
+			10,
+			false,
+			false,
+		},
+
+		// Has match.
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueDouble(10),
+			},
+			"exist",
+			logql.OpEq,
+			10,
+			true,
+			false,
+		},
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueInt(10),
+			},
+			"exist",
+			logql.OpEq,
+			10,
+			true,
+			false,
+		},
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr("10"),
+			},
+			"exist",
+			logql.OpEq,
+			10,
+			true,
+			false,
+		},
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueDouble(9),
+			},
+			"exist",
+			logql.OpNotEq,
+			10,
+			true,
+			false,
+		},
+		// Greater than.
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueDouble(11),
+			},
+			"exist",
+			logql.OpGt,
+			10,
+			true,
+			false,
+		},
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueDouble(11),
+			},
+			"exist",
+			logql.OpGte,
+			10,
+			true,
+			false,
+		},
+		// Less than.
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueDouble(9),
+			},
+			"exist",
+			logql.OpLt,
+			10,
+			true,
+			false,
+		},
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueDouble(9),
+			},
+			"exist",
+			logql.OpLte,
+			10,
+			true,
+			false,
+		},
+
+		// Parsing error.
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr(`foo`),
+			},
+			"exist",
+			logql.OpEq,
+			10,
+			false,
+			true,
+		},
+	}
+	for i, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("Test%d", i+1), func(t *testing.T) {
+			set := newLabelSet()
+			set.labels = tt.input
+
+			f, err := buildNumberLabelFilter(&logql.NumberFilter{
+				Label: logql.Label(tt.label),
+				Op:    tt.op,
+				Value: tt.value,
+			})
+			require.NoError(t, err)
+
+			newLine, gotOk := f.Process(0, ``, set)
+			// Ensure that extractor does not change the line.
+			require.Equal(t, ``, newLine)
+
+			if tt.wantFilterErr {
+				_, ok := set.GetError()
+				require.True(t, ok)
+				return
+			}
+			errMsg, ok := set.GetError()
+			require.False(t, ok, "got error: %s", errMsg)
+
+			require.Equal(t, tt.wantOk, gotOk)
+		})
+	}
+}
+
 func TestIPLabelFilter(t *testing.T) {
 	tests := []struct {
-		input   map[string]pcommon.Value
-		label   string
-		pattern string
-		wantOk  bool
+		input         map[string]pcommon.Value
+		label         string
+		pattern       string
+		wantOk        bool
+		wantFilterErr bool
 	}{
 		// No label.
 		{
@@ -23,17 +420,20 @@ func TestIPLabelFilter(t *testing.T) {
 			`not_exist`,
 			"192.168.1.1",
 			false,
+			false,
 		},
 		{
 			map[string]pcommon.Value{},
 			`not_exist`,
 			"192.168.1.0-192.168.1.255",
 			false,
+			false,
 		},
 		{
 			map[string]pcommon.Value{},
 			`not_exist`,
 			"192.168.1.0/24",
+			false,
 			false,
 		},
 
@@ -42,25 +442,28 @@ func TestIPLabelFilter(t *testing.T) {
 			map[string]pcommon.Value{
 				"exist": pcommon.NewValueStr(`192.168.1.1`),
 			},
-			`exist`,
+			"exist",
 			"192.168.1.1",
 			true,
+			false,
 		},
 		{
 			map[string]pcommon.Value{
 				"exist": pcommon.NewValueStr(`192.168.1.1`),
 			},
-			`exist`,
+			"exist",
 			"192.168.1.0-192.168.1.255",
 			true,
+			false,
 		},
 		{
 			map[string]pcommon.Value{
 				"exist": pcommon.NewValueStr(`192.168.1.1`),
 			},
-			`exist`,
+			"exist",
 			"192.168.1.0/24",
 			true,
+			false,
 		},
 
 		// No match.
@@ -68,25 +471,39 @@ func TestIPLabelFilter(t *testing.T) {
 			map[string]pcommon.Value{
 				"exist": pcommon.NewValueStr(`127.0.0.1`),
 			},
-			`exist`,
+			"exist",
 			"192.168.1.1",
 			false,
+			false,
 		},
 		{
 			map[string]pcommon.Value{
 				"exist": pcommon.NewValueStr(`127.0.0.1`),
 			},
-			`exist`,
+			"exist",
 			"192.168.1.0-192.168.1.255",
 			false,
+			false,
 		},
 		{
 			map[string]pcommon.Value{
 				"exist": pcommon.NewValueStr(`127.0.0.1`),
 			},
-			`exist`,
+			"exist",
 			"192.168.1.0/24",
 			false,
+			false,
+		},
+
+		// Parsing error.
+		{
+			map[string]pcommon.Value{
+				"exist": pcommon.NewValueStr(`127.0.0.`),
+			},
+			"exist",
+			"192.168.1.0",
+			false,
+			true,
 		},
 	}
 	for i, tt := range tests {
@@ -110,10 +527,19 @@ func TestIPLabelFilter(t *testing.T) {
 				})
 				require.NoError(t, err)
 
-				newLine, ok := f.Process(0, ``, set)
+				newLine, gotOk := f.Process(0, ``, set)
 				// Ensure that extractor does not change the line.
 				require.Equal(t, ``, newLine)
-				require.Equal(t, cse.wantOk, ok)
+
+				if tt.wantFilterErr {
+					_, ok := set.GetError()
+					require.True(t, ok)
+					return
+				}
+				errMsg, ok := set.GetError()
+				require.False(t, ok, "got error: %s", errMsg)
+
+				require.Equal(t, cse.wantOk, gotOk)
 			}
 		})
 	}
