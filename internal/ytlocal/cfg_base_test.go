4a4f42185322c175427ebaa4c825d5863c55f065
Aleksandr Razumov <ernado@go-faster.org>
2023-07-18 19:38:19 +0300

    test(ytlocal): init

--- internal/ytlocal/cfg_base_test.go
+++ internal/ytlocal/cfg_base_test.go
@@ -1,5 +1,14 @@
 package ytlocal
 
+import (
+	"bytes"
+	"testing"
+
+	"github.com/go-faster/sdk/gold"
+	"github.com/stretchr/testify/require"
+	"go.ytsaurus.tech/yt/go/yson"
+)
+
 func newBaseServer() BaseServer {
 	return BaseServer{
 		RPCPort:        9015,
@@ -36,4 +45,13 @@ func newBaseServer() BaseServer {
 		},
 		Logging: Logging{},
 	}
+}
+
+func encode(t *testing.T, name string, v any) {
+	t.Helper()
+	out := new(bytes.Buffer)
+	w := yson.NewWriterFormat(out, yson.FormatPretty)
+	e := yson.NewEncoderWriter(w)
+	require.NoError(t, e.Encode(v))
+	gold.Str(t, out.String(), name+".yson")
 }
--- internal/ytlocal/ytlocal_test.go
+++ internal/ytlocal/ytlocal_test.go
@@ -1,19 +1,58 @@
 package ytlocal
 
 import (
-	"bytes"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
-
-	"github.com/go-faster/sdk/gold"
-	"github.com/stretchr/testify/require"
-	"go.ytsaurus.tech/yt/go/yson"
 )
 
-func encode(t *testing.T, name string, v any) {
-	t.Helper()
-	out := new(bytes.Buffer)
-	w := yson.NewWriterFormat(out, yson.FormatPretty)
-	e := yson.NewEncoderWriter(w)
-	require.NoError(t, e.Encode(v))
-	gold.Str(t, out.String(), name+".yson")
+func TestRun(t *testing.T) {
+	runDir := t.TempDir()
+
+	singleBinary := "ytserver-all"
+	singleBinaryPath, err := exec.LookPath(singleBinary)
+	if err != nil {
+		t.Skipf("Binary %q not found in $PATH", singleBinary)
+	}
+
+	// Ensure that all binaries are available.
+	//
+	// See TryProgram here for list:
+	// https://github.com/ytsaurus/ytsaurus/blob/d8cc9c52b6fd94b352a4264579dd89a75aae9b38/yt/yt/server/all/main.cpp#L49-L74
+	binaries := map[string]string{}
+	for _, name := range []string{
+		"master",
+		"clock",
+		"http-proxy",
+		"node",
+		"job-proxy",
+		"exec",
+		"tools",
+		"scheduler",
+		"controller-agent",
+		"log-tailer",
+		"discovery",
+		"timestamp-provider",
+		"master-cache",
+		"cell-balancer",
+		"queue-agent",
+		"tablet-balancer",
+		"cypress-proxy",
+		"query-tracker",
+		"tcp-proxy",
+	} {
+		binaryPath := "ytserver-" + name
+		p, err := exec.LookPath(binaryPath)
+		if err == nil {
+			binaries[name] = p
+			continue
+		}
+		// Create link.
+		binaryPath = filepath.Join(runDir, binaryPath)
+		if err := os.Symlink(singleBinaryPath, binaryPath); err != nil {
+			t.Fatalf("failed to create link: %v", err)
+		}
+		binaries[name] = binaryPath
+	}
 }
