1dea7e9f6317964db7fe853e2ea3ca7f91d376b0
Aleksandr Razumov <ernado@go-faster.org>
2023-07-22 13:23:45 +0300

    feat(ytlocal): add base server factory

--- internal/ytlocal/ytlocal_test.go
+++ internal/ytlocal/ytlocal_test.go
@@ -27,6 +27,89 @@ func (p *PortAllocator) RequireAllocate(tb testing.TB) int {
 	return n
 }
 
+func baseServerFactory(tpl BaseServer, ports *PortAllocator) func(tb testing.TB) BaseServer {
+	return func(tb testing.TB) BaseServer {
+		tb.Helper()
+		return BaseServer{
+			RPCPort:        ports.RequireAllocate(tb),
+			MonitoringPort: ports.RequireAllocate(tb),
+
+			TimestampProvider: tpl.TimestampProvider,
+			ClusterConnection: tpl.ClusterConnection,
+			Logging:           tpl.Logging,
+			AddressResolver:   tpl.AddressResolver,
+		}
+	}
+}
+
+// Component is a helper to run ytserver components.
+type Component[T any] struct {
+	Name   string
+	Binary string
+	RunDir string
+	Config T
+	TB     testing.TB
+}
+
+// Run runs the component.
+func (c Component[T]) Run(ctx context.Context) error {
+	data, err := yson.Marshal(c.Config)
+	if err != nil {
+		return errors.Wrap(err, "marshal config")
+	}
+
+	cfgName := c.Name + ".yson"
+	cfgPath := filepath.Join(c.RunDir, cfgName)
+	if err := os.WriteFile(cfgPath, data, 0644); err != nil {
+		return errors.Wrap(err, "write config")
+	}
+
+	args := []string{
+		"--config", cfgPath,
+	}
+
+	ctx, timeoutCancel := context.WithTimeout(interrupted(), time.Second*10)
+	defer timeoutCancel()
+	ctx, cancel := context.WithCancelCause(ctx)
+	defer func() {
+		cancel(context.Canceled)
+	}()
+
+	g, ctx := errgroup.WithContext(ctx)
+	cmd := exec.CommandContext(ctx, c.Binary, args...)
+	r, w := io.Pipe()
+	cmd.Stderr = w
+	if err := cmd.Start(); err != nil {
+		return errors.Wrap(err, "start")
+	}
+	errFound := errors.New("found")
+
+	g.Go(func() error {
+		defer func() { _ = r.Close() }()
+		scanner := bufio.NewScanner(r)
+		for scanner.Scan() {
+			if strings.Contains(scanner.Text(), "Leader active") {
+				c.TB.Log("Leader active")
+				cancel(errFound)
+				return nil
+			}
+		}
+		return scanner.Err()
+	})
+	g.Go(func() error {
+		defer func() { _ = w.Close() }()
+		if err := cmd.Wait(); err != nil {
+			if errors.Is(context.Cause(ctx), errFound) {
+				return nil
+			}
+			return err
+		}
+		return nil
+	})
+
+	return g.Wait()
+}
+
 func TestRun(t *testing.T) {
 	if ok, _ := strconv.ParseBool(os.Getenv("YT_LOCAL_TEST")); !ok {
 		t.Skip("Set YT_LOCAL_TEST=1")
@@ -88,206 +171,183 @@ func TestRun(t *testing.T) {
 		binaries[name] = binaryPath
 	}
 
-	// Try running master.
-	masterPort := ports.RequireAllocate(t)
-	masterMonitoringPort := ports.RequireAllocate(t)
-	const cellID = "a3c51a55-ffffffff-259-ffffffff"
-
-	masterAddr := fmt.Sprintf("%s:%d", localhost, masterPort)
-	cfg := Master{
-		BaseServer: BaseServer{
-			RPCPort:        masterPort,
-			MonitoringPort: masterMonitoringPort,
-			AddressResolver: AddressResolver{
-				Retries:       3,
-				EnableIPv4:    true,
-				EnableIPv6:    false,
-				LocalhostFQDN: localhost,
-			},
-			TimestampProvider: Connection{
-				Addresses:       []string{masterAddr},
-				SoftBackoffTime: 100,
-				HardBackoffTime: 100,
-				UpdatePeriod:    500,
-			},
-			ClusterConnection: ClusterConnection{
-				ClusterName: "test",
-				CellDirectory: CellDirectory{
-					SoftBackoffTime:           100,
-					HardBackoffTime:           100,
-					EnablePeerPolling:         true,
-					PeerPollingPeriod:         500,
-					PeerPollingPeriodSplay:    100,
-					PeerPollingRequestTimeout: 100,
-					RediscoverPeriod:          5_000,
-					RediscoverSplay:           500,
-				},
-				DiscoveryConnection: Connection{
-					Addresses: []string{masterAddr},
-				},
-				TimestampProvider: Connection{
-					Addresses:       []string{masterAddr},
-					SoftBackoffTime: 100,
-					HardBackoffTime: 100,
-					UpdatePeriod:    500,
-				},
-				PrimaryMaster: Connection{
-					Addresses:                 []string{masterAddr},
-					CellID:                    cellID,
-					RetryBackoffTime:          100,
-					RetryAttempts:             100,
-					RPCTimeout:                25_000,
-					SoftBackoffTime:           100,
-					HardBackoffTime:           100,
-					EnablePeerPolling:         true,
-					PeerPollingPeriod:         500,
-					PeerPollingPeriodSplay:    100,
-					PeerPollingRequestTimeout: 100,
-					RediscoverPeriod:          5_000,
-					RediscoverSplay:           500,
+	const (
+		cellID      = "a3c51a55-ffffffff-259-ffffffff"
+		clusterName = "test"
+	)
+	var (
+		masterPort           = ports.RequireAllocate(t)
+		masterMonitoringPort = ports.RequireAllocate(t)
+		masterAddr           = fmt.Sprintf("%s:%d", localhost, masterPort)
+		cfgTimestampProvider = Connection{
+			Addresses:       []string{masterAddr},
+			SoftBackoffTime: 100,
+			HardBackoffTime: 100,
+			UpdatePeriod:    500,
+		}
+		cfgPrimaryMaster = Connection{
+			Addresses:                 []string{masterAddr},
+			CellID:                    cellID,
+			RetryBackoffTime:          100,
+			RetryAttempts:             100,
+			RPCTimeout:                25_000,
+			SoftBackoffTime:           100,
+			HardBackoffTime:           100,
+			EnablePeerPolling:         true,
+			PeerPollingPeriod:         500,
+			PeerPollingPeriodSplay:    100,
+			PeerPollingRequestTimeout: 100,
+			RediscoverPeriod:          5_000,
+			RediscoverSplay:           500,
+		}
+		cfgAddressResolver = AddressResolver{
+			Retries:       3,
+			EnableIPv4:    true,
+			EnableIPv6:    false,
+			LocalhostFQDN: localhost,
+		}
+		cfgCellDirectory = CellDirectory{
+			SoftBackoffTime:           100,
+			HardBackoffTime:           100,
+			EnablePeerPolling:         true,
+			PeerPollingPeriod:         500,
+			PeerPollingPeriodSplay:    100,
+			PeerPollingRequestTimeout: 100,
+			RediscoverPeriod:          5_000,
+			RediscoverSplay:           500,
+		}
+		cfgDiscoveryConnection = Connection{
+			Addresses: []string{masterAddr},
+		}
+		cfgLogging = Logging{
+			AbortOnAlert:           true,
+			CompressionThreadCount: 4,
+			Writers: map[string]LoggingWriter{
+				"stderr": {
+					Format:     LogFormatPlainText,
+					WriterType: LogWriterTypeStderr,
 				},
 			},
-			Logging: Logging{
-				AbortOnAlert:           true,
-				CompressionThreadCount: 4,
-				Writers: map[string]LoggingWriter{
-					"stderr": {
-						Format:     LogFormatPlainText,
-						WriterType: LogWriterTypeStderr,
-					},
-				},
-				Rules: []LoggingRule{
-					{
-						Writers:  []string{"stderr"},
-						MinLevel: LogLevelInfo,
-					},
+			Rules: []LoggingRule{
+				{
+					Writers:  []string{"stderr"},
+					MinLevel: LogLevelInfo,
 				},
 			},
-		},
-		EnableProvisionLock: false,
-		UseNewHydra:         true,
-		ChunkManger: ChunkManger{
-			AllowMultipleErasurePartsPerNode: true,
-		},
-		Changelogs: MasterChangelogs{
-			FlushPeriod: 10,
-			EnableSync:  false,
-			IOEngine: IOEngine{
-				EnableSync: false,
-			},
-			Path: filepath.Join(runDir, "changelogs"),
-		},
-		ObjectService: ObjectService{
-			EnableLocalReadExecutor: true,
-			EnableLocalReadBusyWait: false,
-		},
-		EnableTimestampManager: true,
-		TimestampManager: TimestampManager{
+		}
+		cfgClusterConnection = ClusterConnection{
+			ClusterName:         clusterName,
+			CellDirectory:       cfgCellDirectory,
+			PrimaryMaster:       cfgPrimaryMaster,
+			TimestampProvider:   cfgTimestampProvider,
+			DiscoveryConnection: cfgDiscoveryConnection,
+		}
+		cfgBaseServer = BaseServer{
+			RPCPort:           masterPort,
+			MonitoringPort:    masterMonitoringPort,
+			AddressResolver:   cfgAddressResolver,
+			TimestampProvider: cfgTimestampProvider,
+			ClusterConnection: cfgClusterConnection,
+			Logging:           cfgLogging,
+		}
+		cfgTCPDispatcher = TCPDispatcher{
+			ThreadPoolSize: 2,
+		}
+		cfgSolomonExporter = SolomonExporter{
+			GridStep: 1_000,
+		}
+		cfgCypressAnnotations = CypressAnnotations{
+			YTEnvIndex: 0,
+		}
+		cfgYPServiceDiscovery = YPServiceDiscovery{
+			Enable: false,
+		}
+		cfgTimestampManager = TimestampManager{
 			CommitAdvance:      3_000,
 			RequestBackoffTime: 10,
 			CalibrationPeriod:  10,
-		},
-		HiveManager: HiveManager{
-			PingPeriod:     1_000,
-			IdlePostPeriod: 1_000,
-		},
-		PrimaryMaster: Connection{
-			Addresses: []string{masterAddr},
-			CellID:    cellID,
-		},
-		HydraManager: HydraManager{
-			SnapshotBackgroundThreadCount: 4,
-			LeaderSyncDelay:               0,
-			MinimizeCommitLatency:         true,
-			LeaderLeaseCheckPeriod:        100,
-			LeaderLeaseTimeout:            20_000,
-			DisableLeaderLeaseGraceDelay:  true,
-			InvariantsCheckProbability:    0.005,
-			ResponseKeeper: ResponseKeeper{
-				EnableWarmup:   false,
-				ExpirationTime: 25_000,
-				WarmupTime:     30_000,
-			},
-			MaxChangelogDataSize: 268435456,
-		},
-		YPServiceDiscovery: YPServiceDiscovery{
-			Enable: false,
-		},
-		RPCDispatcher: RPCDispatcher{
-			CompressionPoolSize: 1,
-			HeavyPoolSize:       1,
-		},
-		ChunkClientDispatcher: ChunkClientDispatcher{
-			ChunkReaderPoolSize: 1,
-		},
-		TCPDispatcher: TCPDispatcher{
-			ThreadPoolSize: 4,
-		},
-		SolomonExporter: SolomonExporter{
-			GridStep: 1_000,
-		},
-		CypressAnnotations: CypressAnnotations{
-			YTEnvIndex: 0,
-		},
-		CypressManager: CypressManager{
+		}
+		cfgCypressManager = CypressManager{
 			DefaultJournalWriteQuorum:       1,
 			DefaultJournalReadQuorum:        1,
 			DefaultFileReplicationFactor:    1,
 			DefaultJournalReplicationFactor: 1,
 			DefaultTableReplicationFactor:   1,
-		},
-		EnableRefCountedTrackerProfiling: false,
-		EnableResourceTracker:            false,
-		Snapshots: MasterSnapshots{
-			Path: filepath.Join(runDir, "snapshots"),
+		}
+		cfgChunkClientDispatcher = ChunkClientDispatcher{
+			ChunkReaderPoolSize: 1,
+		}
+		cfgRPCDispatcher = RPCDispatcher{
+			CompressionPoolSize: 1,
+			HeavyPoolSize:       1,
+		}
+		cfgHiveManager = HiveManager{
+			PingPeriod:     1_000,
+			IdlePostPeriod: 1_000,
+		}
+		cfgObjectService = ObjectService{
+			EnableLocalReadExecutor: true,
+			EnableLocalReadBusyWait: false,
+		}
+		cfgChunkManager = ChunkManger{
+			AllowMultipleErasurePartsPerNode: true,
+		}
+	)
+	master := Component[Master]{
+		TB:     t,
+		RunDir: runDir,
+		Name:   "master",
+		Binary: binaries["master"],
+		Config: Master{
+			BaseServer: cfgBaseServer,
+
+			ChunkManger:           cfgChunkManager,
+			ObjectService:         cfgObjectService,
+			TimestampManager:      cfgTimestampManager,
+			HiveManager:           cfgHiveManager,
+			PrimaryMaster:         cfgPrimaryMaster,
+			YPServiceDiscovery:    cfgYPServiceDiscovery,
+			RPCDispatcher:         cfgRPCDispatcher,
+			ChunkClientDispatcher: cfgChunkClientDispatcher,
+			TCPDispatcher:         cfgTCPDispatcher,
+			SolomonExporter:       cfgSolomonExporter,
+			CypressAnnotations:    cfgCypressAnnotations,
+			CypressManager:        cfgCypressManager,
+
+			EnableRefCountedTrackerProfiling: false,
+			EnableResourceTracker:            false,
+			EnableProvisionLock:              false,
+			UseNewHydra:                      true,
+			EnableTimestampManager:           true,
+
+			Snapshots: MasterSnapshots{
+				Path: filepath.Join(runDir, "snapshots"),
+			},
+			HydraManager: HydraManager{
+				SnapshotBackgroundThreadCount: 4,
+				LeaderSyncDelay:               0,
+				MinimizeCommitLatency:         true,
+				LeaderLeaseCheckPeriod:        100,
+				LeaderLeaseTimeout:            20_000,
+				DisableLeaderLeaseGraceDelay:  true,
+				InvariantsCheckProbability:    0.005,
+				ResponseKeeper: ResponseKeeper{
+					EnableWarmup:   false,
+					ExpirationTime: 25_000,
+					WarmupTime:     30_000,
+				},
+				MaxChangelogDataSize: 268435456,
+			},
+			Changelogs: MasterChangelogs{
+				FlushPeriod: 10,
+				EnableSync:  false,
+				IOEngine: IOEngine{
+					EnableSync: false,
+				},
+				Path: filepath.Join(runDir, "changelogs"),
+			},
 		},
 	}
-	data, err := yson.Marshal(cfg)
-	require.NoError(t, err)
 
-	cfgPath := filepath.Join(runDir, "master.yson")
-	require.NoError(t, os.WriteFile(cfgPath, data, 0644))
-
-	args := []string{
-		"--config", cfgPath,
-	}
-
-	ctx, timeoutCancel := context.WithTimeout(interrupted(), time.Second*10)
-	defer timeoutCancel()
-	ctx, cancel := context.WithCancelCause(ctx)
-	defer func() {
-		cancel(context.Canceled)
-	}()
-
-	g, ctx := errgroup.WithContext(ctx)
-	cmd := exec.CommandContext(ctx, binaries["master"], args...)
-	r, w := io.Pipe()
-	cmd.Stderr = w
-	require.NoError(t, cmd.Start())
-	errFound := errors.New("found")
-
-	g.Go(func() error {
-		defer func() { _ = r.Close() }()
-		scanner := bufio.NewScanner(r)
-		for scanner.Scan() {
-			if strings.Contains(scanner.Text(), "Leader active") {
-				t.Log("Leader active")
-				cancel(errFound)
-				return nil
-			}
-		}
-		return scanner.Err()
-	})
-	g.Go(func() error {
-		defer func() { _ = w.Close() }()
-		if err := cmd.Wait(); err != nil {
-			if errors.Is(context.Cause(ctx), errFound) {
-				return nil
-			}
-			return err
-		}
-		return nil
-	})
-
-	require.NoError(t, g.Wait())
+	require.NoError(t, master.Run(context.Background()))
 }
