b9a3cf801bd8ca0d9360b12dddb292481436dcc4
tdakkota <tanc13@yandex.ru>
2023-10-02 19:06:36 +0300

    feat(metricstorage): add schema for metrics

---
+++ internal/metricstorage/schema.go
@@ -0,0 +1,78 @@
+package metricstorage
+
+import (
+	"go.ytsaurus.tech/yt/go/schema"
+
+	"github.com/go-faster/oteldb/internal/otelstorage"
+)
+
+// Point is a data structure for metric points.
+type Point struct {
+	Metric        string                `json:"metric_name" yson:"metric_name"`
+	ResourceHash  string                `json:"resource_hash" yson:"resource_hash"`
+	AttributeHash string                `json:"attr_hash" yson:"attr_hash"`
+	Timestamp     otelstorage.Timestamp `json:"timestamp" yson:"timestamp"`
+	Point         float64               `json:"point" yson:"point"`
+}
+
+// YTSchema returns YTsaurus table schema for this structure.
+func (Point) YTSchema() schema.Schema {
+	var (
+		tsType   = schema.TypeUint64
+		hashType = schema.TypeString
+	)
+
+	return schema.Schema{
+		Columns: []schema.Column{
+			{Name: "metric_name", ComplexType: schema.TypeString, SortOrder: schema.SortAscending},
+			{Name: "resource_hash", ComplexType: hashType, SortOrder: schema.SortAscending},
+			{Name: "attr_hash", ComplexType: hashType, SortOrder: schema.SortAscending},
+			{Name: "timestamp", ComplexType: tsType, SortOrder: schema.SortAscending},
+			{Name: "point", ComplexType: schema.TypeFloat64},
+		},
+	}
+}
+
+// Resource is a data structure for resource.
+type Resource struct {
+	Hash  string            `json:"hash" yson:"hash"`
+	Attrs otelstorage.Attrs `json:"attrs" yson:"attrs"`
+}
+
+// YTSchema returns YTsaurus table schema for this structure.
+func (Resource) YTSchema() schema.Schema {
+	var (
+		hashType  = schema.TypeString
+		attrsType = schema.Optional{Item: schema.TypeAny}
+	)
+
+	return schema.Schema{
+		Columns: []schema.Column{
+			{Name: "hash", ComplexType: hashType, SortOrder: schema.SortAscending},
+			{Name: "attrs", ComplexType: attrsType},
+		},
+	}
+}
+
+// Attributes is a data structure for attributes.
+type Attributes struct {
+	Metric string            `json:"metric_name" yson:"metric_name"`
+	Hash   string            `json:"hash" yson:"hash"`
+	Attrs  otelstorage.Attrs `json:"attrs" yson:"attrs"`
+}
+
+// YTSchema returns YTsaurus table schema for this structure.
+func (Attributes) YTSchema() schema.Schema {
+	var (
+		hashType  = schema.TypeString
+		attrsType = schema.Optional{Item: schema.TypeAny}
+	)
+
+	return schema.Schema{
+		Columns: []schema.Column{
+			{Name: "metric_name", ComplexType: schema.TypeString, SortOrder: schema.SortAscending},
+			{Name: "hash", ComplexType: hashType, SortOrder: schema.SortAscending},
+			{Name: "attrs", ComplexType: attrsType},
+		},
+	}
+}
