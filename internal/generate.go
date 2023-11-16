eeba730e01b311b5964b1426ca6e9d41f3b03a6f
tdakkota <tanc13@yandex.ru>
2023-11-16 23:18:50 +0300

    chore: use ogen config

--- internal/generate.go
+++ internal/generate.go
@@ -1,10 +1,10 @@
 // Package internal contains go:generate annotations.
 package internal
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -generate-tests -v --convenient-errors --target tempoapi     --package tempoapi     ../_oas/tempo.yml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -generate-tests -v --convenient-errors --target lokiapi      --package lokiapi      ../_oas/loki.yml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -generate-tests -v --convenient-errors --target promapi      --package promapi      ../_oas/prometheus.yml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -generate-tests -v --convenient-errors --target pyroscopeapi --package pyroscopeapi ../_oas/pyroscope.yml
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -generate-tests -v --convenient-errors --target sentryapi    --package sentryapi    ../_oas/sentry.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --target tempoapi     --package tempoapi     ../_oas/tempo.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --target lokiapi      --package lokiapi      ../_oas/loki.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --target promapi      --package promapi      ../_oas/prometheus.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --target pyroscopeapi --package pyroscopeapi ../_oas/pyroscope.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --target sentryapi    --package sentryapi    ../_oas/sentry.yml
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --convenient-errors --target yqlclient/ytqueryapi --package ytqueryapi    ../_oas/ytquery.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -v --target yqlclient/ytqueryapi --package ytqueryapi    ../_oas/ytquery.yml
