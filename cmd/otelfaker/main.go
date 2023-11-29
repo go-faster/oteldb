07718ef0e3a6fa49e3ddcb7173f3c8e486f950e0
Aleksandr Razumov <ernado@go-faster.org>
2023-11-29 15:19:48 +0300

    feat(otelfaker): improve

--- cmd/otelfaker/main.go
+++ cmd/otelfaker/main.go
@@ -26,38 +26,50 @@ func getLogs(ctx context.Context, tracer trace.Tracer, rnd *rand.Rand, now time.
 		spanContext = span.SpanContext()
 		traceID     = spanContext.TraceID()
 		spanID      = spanContext.SpanID()
+		ld          = plog.NewLogs()
+		rl          = ld.ResourceLogs().AppendEmpty()
 	)
-	ld := plog.NewLogs()
-	rl := ld.ResourceLogs().AppendEmpty()
-	rl.Resource().Attributes().PutStr("host.name", "testHost")
+	{
+		a := rl.Resource().Attributes()
+		a.PutStr("service.name", "testService")
+		a.PutStr("service.version", "1.0.0")
+		a.PutStr("service.instance.id", "testInstance")
+		a.PutStr("service.namespace", "testNamespace")
+		a.PutStr("host.name", "testHost")
+	}
 	rl.SetSchemaUrl("resource_schema")
 	il := rl.ScopeLogs().AppendEmpty()
-	il.Scope().SetName("name")
-	il.Scope().SetVersion("version")
-	il.Scope().Attributes().PutStr("oteldb.name", "testDB")
-	il.Scope().SetDroppedAttributesCount(1)
+	{
+		s := il.Scope()
+		s.SetName("name")
+		s.SetVersion("version")
+		s.Attributes().PutStr("oteldb.name", "testDB")
+		s.SetDroppedAttributesCount(1)
+	}
 	il.SetSchemaUrl("scope_schema")
 	lg := il.LogRecords().AppendEmpty()
 	lg.SetSeverityNumber(plog.SeverityNumber(logspb.SeverityNumber_SEVERITY_NUMBER_INFO))
 	lg.SetSeverityText("Info")
-	lg.SetFlags(plog.LogRecordFlags(logspb.LogRecordFlags_LOG_RECORD_FLAGS_DO_NOT_USE))
 	lg.SetTraceID(pcommon.TraceID(traceID))
 	lg.SetSpanID(pcommon.SpanID(spanID))
 	lg.Body().SetStr("hello world")
 	lg.SetTimestamp(pcommon.Timestamp(now.UnixNano()))
 	lg.SetObservedTimestamp(pcommon.Timestamp(now.UnixNano()))
-	lg.Attributes().PutStr("sdkVersion", "1.0.1")
-	lg.Attributes().PutStr("http.method", "GET")
-	lg.Attributes().PutBool("http.server", true)
-	lg.Attributes().PutInt("http.status_code", 200)
-	if rnd.Float32() < 0.5 {
-		lg.Attributes().PutStr("http.url", "https://example.com")
-		lg.Attributes().PutStr("http.status_text", "OK")
-	} else {
-		lg.Attributes().PutStr("http.user_agent", "test-agent")
+	{
+		a := lg.Attributes()
+		a.PutStr("sdkVersion", "1.0.1")
+		a.PutStr("http.method", "GET")
+		a.PutBool("http.server", true)
+		a.PutInt("http.status_code", 200)
+		if rnd.Float32() < 0.5 {
+			a.PutStr("http.url", "https://example.com")
+			a.PutStr("http.status_text", "OK")
+		} else {
+			a.PutStr("http.user_agent", "test-agent")
+		}
+		a.PutDouble("http.duration_seconds", 1.1054)
+		a.PutInt("http.duration", (time.Second + time.Millisecond*105).Nanoseconds())
 	}
-	lg.Attributes().PutDouble("http.duration_seconds", 1.1054)
-	lg.Attributes().PutInt("http.duration", (time.Second + time.Millisecond*105).Nanoseconds())
 	lg.SetFlags(plog.DefaultLogRecordFlags.WithIsSampled(true))
 	return ld
 }
