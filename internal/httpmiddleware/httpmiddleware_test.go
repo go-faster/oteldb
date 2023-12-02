c9d758370d7333ecaf74913e467075bdfed2fd13
Aleksandr Razumov <ernado@go-faster.org>
2023-12-02 16:16:50 +0300

    test(httpmidleware): add order test

--- internal/httpmiddleware/httpmiddleware_test.go
+++ internal/httpmiddleware/httpmiddleware_test.go
@@ -2,15 +2,22 @@
 package httpmiddleware
 
 import (
+	"context"
 	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"testing"
 
 	"github.com/go-faster/sdk/zctx"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"go.uber.org/zap/zaptest/observer"
+
+	"github.com/go-faster/oteldb/integration"
 )
 
 type testHandler struct{}
@@ -129,4 +136,49 @@ func TestWrap(t *testing.T) {
 	)
 	result.ServeHTTP(nil, nil)
 	require.Equal(t, []int{1, 2, 3}, calls)
+}
+
+func TestInstrumentation(t *testing.T) {
+	provider := integration.NewProvider()
+	tracer := provider.Tracer("test")
+	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		t.Logf("Handler(ctx).IsValid(): %v", trace.SpanContextFromContext(ctx).IsValid())
+		assert.True(t, trace.SpanContextFromContext(ctx).IsValid())
+		_, span := tracer.Start(r.Context(), "Handler")
+		defer span.End()
+		w.WriteHeader(http.StatusOK)
+	})
+	h := Wrap(fn,
+		otelhttp.NewMiddleware("otelhttp.Middleware",
+			otelhttp.WithTracerProvider(provider),
+		),
+		func(handler http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				ctx := r.Context()
+				sc := trace.SpanContextFromContext(ctx)
+				t.Logf("after(ctx).IsValid(): %v (%s)", sc.IsValid(), sc.TraceID())
+				assert.True(t, sc.IsValid(), "Middleware span should be valid")
+
+				ctx, span := tracer.Start(ctx, "After")
+				defer span.End()
+				handler.ServeHTTP(w, r.WithContext(ctx))
+			})
+		},
+	)
+	rw := httptest.NewRecorder()
+	req := &http.Request{
+		Method: http.MethodGet,
+		URL: &url.URL{
+			Path: "/foo",
+		},
+	}
+	h.ServeHTTP(rw, req.WithContext(context.Background()))
+	require.Equal(t, http.StatusOK, rw.Code)
+	provider.Flush()
+	spans := provider.Exporter.GetSpans()
+	assert.Len(t, spans, 3)
+	for _, s := range spans {
+		t.Logf("%s [%s]", s.Name, s.SpanContext.TraceID())
+	}
 }
