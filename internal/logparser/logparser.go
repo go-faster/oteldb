8522cbff05b32080341f0672af2d3ad352a4ff78
Aleksandr Razumov <ernado@go-faster.org>
2023-12-05 13:18:41 +0300

    chore(logparser): add TODO

--- internal/logparser/logparser.go
+++ internal/logparser/logparser.go
@@ -3,7 +3,13 @@ package logparser
 
 // Parser parses logs.
 type Parser interface {
+	// Parse parses data and returns a line.
+	//
+	// TODO: refactor to `Parse(data []byte, target *logstorage.Record) error`
 	Parse(data []byte) (*Line, error)
+	// Detect whether data is parsable.
+	//
+	// TODO: refactor to `Detect(data []byte) bool`
 	Detect(line string) bool
 	String() string
 }
