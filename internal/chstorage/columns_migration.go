4a525beccaf61a3318eec469d3f9b6d0ceaad08c
Aleksandr Razumov <ernado@go-faster.org>
2023-12-05 17:06:05 +0300

    feat(chstorage): add table re-create

---
+++ internal/chstorage/columns_migration.go
@@ -0,0 +1,39 @@
+package chstorage
+
+import (
+	"github.com/ClickHouse/ch-go/proto"
+)
+
+type migrationColumns struct {
+	table proto.ColStr // table name
+	ddl   proto.ColStr // SHA256(DDL)
+}
+
+func (c *migrationColumns) columns() tableColumns {
+	return []tableColumn{
+		{Name: "table", Data: &c.table},
+		{Name: "ddl", Data: &c.ddl},
+	}
+}
+
+func (c *migrationColumns) Input() proto.Input    { return c.columns().Input() }
+func (c *migrationColumns) Result() proto.Results { return c.columns().Result() }
+
+func newMigrationColumns() *migrationColumns {
+	return &migrationColumns{}
+}
+
+func (c *migrationColumns) Mapping() map[string]string {
+	data := make(map[string]string, c.table.Rows())
+	for i := 0; i < c.table.Rows(); i++ {
+		data[c.table.Row(i)] = c.ddl.Row(i)
+	}
+	return data
+}
+
+func (c *migrationColumns) Save(m map[string]string) {
+	for k, v := range m {
+		c.table.Append(k)
+		c.ddl.Append(v)
+	}
+}
--- internal/chstorage/schema.go
+++ internal/chstorage/schema.go
@@ -2,11 +2,13 @@ package chstorage
 
 import (
 	"context"
+	"crypto/sha256"
 	"fmt"
 
 	"github.com/ClickHouse/ch-go"
 	"github.com/go-faster/errors"
 	"github.com/go-faster/sdk/zctx"
+	"go.uber.org/zap"
 )
 
 // Tables define table names.
@@ -22,6 +24,8 @@ type Tables struct {
 
 	Logs     string
 	LogAttrs string
+
+	Migration string
 }
 
 // Validate checks table names
@@ -51,6 +55,8 @@ func (t *Tables) Each(cb func(name *string) error) error {
 
 		{&t.Logs, "Logs"},
 		{&t.LogAttrs, "LogAttrs"},
+
+		{&t.Migration, "Migration"},
 	} {
 		if err := cb(table.field); err != nil {
 			return errors.Wrapf(err, "table %s", table.fieldName)
@@ -73,11 +79,38 @@ func DefaultTables() Tables {
 
 		Logs:     "logs",
 		LogAttrs: "logs_attrs",
+
+		Migration: "migration",
 	}
 }
 
 type chClient interface {
 	Do(ctx context.Context, q ch.Query) (err error)
+}
+
+func (t Tables) getHashes(ctx context.Context, c chClient) (map[string]string, error) {
+	col := newMigrationColumns()
+	if err := c.Do(ctx, ch.Query{
+		Logger: zctx.From(ctx).Named("ch"),
+		Body:   fmt.Sprintf("SELECT table, ddl FROM %s FINAL", t.Migration),
+		Result: col.Result(),
+	}); err != nil {
+		return nil, errors.Wrap(err, "query")
+	}
+	return col.Mapping(), nil
+}
+
+func (t Tables) saveHashes(ctx context.Context, c chClient, m map[string]string) error {
+	col := newMigrationColumns()
+	col.Save(m)
+	if err := c.Do(ctx, ch.Query{
+		Logger: zctx.From(ctx).Named("ch"),
+		Input:  col.Input(),
+		Body:   col.Input().Into(t.Migration),
+	}); err != nil {
+		return errors.Wrap(err, "query")
+	}
+	return nil
 }
 
 // Create creates tables.
@@ -91,6 +124,21 @@ func (t Tables) Create(ctx context.Context, c chClient) error {
 		query string
 	}
 	for _, s := range []schema{
+		{t.Migration, schemaMigration},
+	} {
+		if err := c.Do(ctx, ch.Query{
+			Logger: zctx.From(ctx).Named("ch"),
+			Body:   fmt.Sprintf(s.query, s.name),
+		}); err != nil {
+			return errors.Wrapf(err, "create %q", s.name)
+		}
+	}
+	hashes, err := t.getHashes(ctx, c)
+	if err != nil {
+		return errors.Wrap(err, "get hashes")
+	}
+
+	for _, s := range []schema{
 		{t.Spans, spansSchema},
 		{t.Tags, tagsSchema},
 
@@ -103,6 +151,23 @@ func (t Tables) Create(ctx context.Context, c chClient) error {
 		{t.Logs, logsSchema},
 		{t.LogAttrs, logAttrsSchema},
 	} {
+		target := fmt.Sprintf("%x", sha256.Sum256([]byte(s.query)))
+		if current, ok := hashes[s.name]; ok && current != target {
+			// HACK: this will DROP all data in the table
+			// TODO: implement ALTER TABLE
+			zctx.From(ctx).Warn("DROPPING TABLE (schema changed!)",
+				zap.String("table", s.name),
+				zap.String("current", current),
+				zap.String("target", target),
+			)
+			if err := c.Do(ctx, ch.Query{
+				Logger: zctx.From(ctx).Named("ch"),
+				Body:   fmt.Sprintf("DROP TABLE %s", s.name),
+			}); err != nil {
+				return errors.Wrapf(err, "drop %q", s.name)
+			}
+		}
+		hashes[s.name] = target
 		if err := c.Do(ctx, ch.Query{
 			Logger: zctx.From(ctx).Named("ch"),
 			Body:   fmt.Sprintf(s.query, s.name),
@@ -110,5 +175,9 @@ func (t Tables) Create(ctx context.Context, c chClient) error {
 			return errors.Wrapf(err, "create %q", s.name)
 		}
 	}
+	if err := t.saveHashes(ctx, c, hashes); err != nil {
+		return errors.Wrap(err, "save hashes")
+	}
+
 	return nil
 }
