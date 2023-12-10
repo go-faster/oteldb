94ea41ebb64405b6d1a35730a56328d300510efa
Aleksandr Razumov <ernado@go-faster.org>
2023-12-10 22:53:06 +0300

    feat(demo): setup exemplars

--- cmd/otelfaker/main.go
+++ cmd/otelfaker/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"context"
+	"flag"
+	"fmt"
+	"os"
 
 	"github.com/go-faster/sdk/app"
-	"go.uber.org/zap"
 )
 
 func main() {
-	app.Run(func(ctx context.Context, lg *zap.Logger, m *app.Metrics) error {
-		<-ctx.Done()
-		return nil
-	})
+	flag.Parse()
+	switch mode := flag.Arg(0); mode {
+	case "server":
+		app.Run(server)
+	case "client":
+		app.Run(client)
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown mode: %s\n", mode)
+		fmt.Fprintf(os.Stderr, "Usage: %s [server|client]\n", os.Args[0])
+		os.Exit(1)
+	}
 }
---
+++ cmd/otelfaker/server.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"context"
+	"math/rand"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/go-faster/sdk/app"
+	"github.com/go-faster/sdk/zctx"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
+)
+
+func server(ctx context.Context, lg *zap.Logger, m *app.Metrics) error {
+	g, ctx := errgroup.WithContext(ctx)
+	mux := http.NewServeMux()
+
+	requestDurations := prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name:    "http_request_duration_seconds",
+		Help:    "A histogram of the HTTP request durations in seconds.",
+		Buckets: prometheus.ExponentialBuckets(0.1, 1.5, 5),
+	})
+
+	registry := prometheus.NewRegistry()
+	registerer := prometheus.WrapRegistererWith(prometheus.Labels{
+		"service_name":      "client",
+		"service_namespace": "demo",
+	}, registry)
+	registerer.MustRegister(
+		requestDurations,
+	)
+
+	// Expose /metrics HTTP endpoint using the created custom registry.
+	mux.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{
+		Registry:          registerer,
+		EnableOpenMetrics: true,
+	}))
+	tracer := m.TracerProvider().Tracer("server")
+	doProcessing := func(ctx context.Context) {
+		ctx, span := tracer.Start(ctx, "doProcessing")
+		defer span.End()
+		// Sleep some random time.
+		duration := time.Duration(100+rand.Intn(100)) * time.Millisecond // #nosec G404
+		time.Sleep(duration)
+		requestDurations.(prometheus.ExemplarObserver).ObserveWithExemplar(duration.Seconds(), prometheus.Labels{
+			"trace_id": span.SpanContext().TraceID().String(),
+			"span_id":  span.SpanContext().SpanID().String(),
+		})
+		zctx.From(ctx).Info("got request", zap.Duration("duration", duration))
+	}
+	mux.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
+		ctx, span := tracer.Start(r.Context(), "hello")
+		defer span.End()
+		doProcessing(ctx)
+		w.WriteHeader(http.StatusOK)
+	})
+	srv := &http.Server{
+		Addr: "0.0.0.0:8080",
+		BaseContext: func(net.Listener) context.Context {
+			return ctx
+		},
+		Handler: otelhttp.NewHandler(mux, "",
+			otelhttp.WithMeterProvider(m.MeterProvider()),
+			otelhttp.WithTracerProvider(m.TracerProvider()),
+			otelhttp.WithSpanNameFormatter(func(operation string, r *http.Request) string {
+				if r.URL.Path == "/api/hello" {
+					return "hello"
+				}
+				return "metrics"
+			}),
+		),
+	}
+	g.Go(func() error {
+		lg.Info("server listening", zap.String("addr", srv.Addr))
+		defer lg.Info("server stopped")
+		return srv.ListenAndServe()
+	})
+	g.Go(func() error {
+		<-ctx.Done()
+		return srv.Shutdown(ctx)
+	})
+	return g.Wait()
+}
