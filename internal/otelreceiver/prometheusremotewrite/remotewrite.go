5f02700ff833ff76525dbe2821e1375ebb9321ec
tdakkota <tanc13@yandex.ru>
2023-12-22 07:31:38 +0300

    chore(prometheusremotewrite): remove unused fields

--- internal/otelreceiver/prometheusremotewrite/remotewrite.go
+++ internal/otelreceiver/prometheusremotewrite/remotewrite.go
@@ -5,10 +5,6 @@ import "go.uber.org/zap"
 
 // Settings defines translation settings.
 type Settings struct {
-	Namespace         string
-	ExternalLabels    map[string]string
-	DisableTargetInfo bool
-	AddMetricSuffixes bool
-	TimeThreshold     int64
-	Logger            zap.Logger
+	TimeThreshold int64
+	Logger        zap.Logger
 }
