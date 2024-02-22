84e41c4a765becf050fcea14a5d2e38f36d3bf2a
tdakkota <tanc13@yandex.ru>
2024-02-22 08:16:58 +0300

    test(prometheusremotewrite): add unmarshal benchmark

--- internal/otelreceiver/prometheusremotewrite/prw_to_metrics_bench_test.go
+++ internal/otelreceiver/prometheusremotewrite/prw_to_metrics_bench_test.go
@@ -37,3 +37,31 @@ func BenchmarkFromTimeSeries(b *testing.B) {
 		require.NoError(b, err)
 	}
 }
+
+func BenchmarkWriteRequestUnmarshal(b *testing.B) {
+	// TODO(tdakkota): move to prompb package.
+	b.ReportAllocs()
+
+	data, err := os.ReadFile(filepath.Join("testdata", "reqs-1k-zstd.rwq"))
+	require.NoError(b, err)
+
+	reader := otelbench.NewReader(bytes.NewReader(data))
+	require.True(b, reader.Decode())
+	compressed := reader.Data()
+	z, err := zstd.NewReader(bytes.NewReader(compressed))
+	require.NoError(b, err)
+	raw, err := io.ReadAll(z)
+	require.NoError(b, err)
+
+	// Pre-allocate request
+	var req prompb.WriteRequest
+	require.NoError(b, req.Unmarshal(raw))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		req.Reset()
+		if err := req.Unmarshal(raw); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
