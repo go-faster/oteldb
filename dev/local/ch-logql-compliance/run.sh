0ddad5bc8e9e831ee717fc88cb3d53928bcacd5b
tdakkota <tanc13@yandex.ru>
2024-04-11 18:55:54 +0300

    ci: add compose files to test LogQL compliance locally

---
+++ dev/local/ch-logql-compliance/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clean_up() {
+  ARG=$?
+  echo ">> Stopping"
+  docker compose down -v
+  exit $ARG
+}
+trap clean_up EXIT
+
+set -e
+
+docker compose up -d --remove-orphans --build --force-recreate
+
+echo ">> Testing oteldb implementation"
+RANGE="1m"
+END="1m"
+go run github.com/go-faster/oteldb/cmd/logql-compliance-tester \
+  -end "${END}" -range "${RANGE}" \
+  -config-file logql-test-queries.yml -config-file test-oteldb.yml \
+  -output-format json -output-file oteldb.result.json || true
