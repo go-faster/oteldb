95dc15b7d1e1dd5f2de555aab342d127dea270ec
tdakkota <tanc13@yandex.ru>
2024-05-23 07:11:37 +0300

    refactor(chstorage): update code due to API changes

--- internal/chstorage/schema_traces.go
+++ internal/chstorage/schema_traces.go
@@ -54,9 +54,11 @@ ORDER BY (service_namespace, service_name, resource, start)
 	(
 		name LowCardinality(String),
 		value String,
-		value_type Enum8(` + valueTypeDDL + `)
+		value_type Enum8(` + valueTypeDDL + `),
+		scope Enum8(` + scopeTypeDDL + `)
 	)
 	ENGINE = ReplacingMergeTree
 	ORDER BY (value_type, name, value);`
 	valueTypeDDL = `'EMPTY' = 0,'STR' = 1,'INT' = 2,'DOUBLE' = 3,'BOOL' = 4,'MAP' = 5,'SLICE' = 6,'BYTES' = 7`
+	scopeTypeDDL = `'NONE' = 0, 'RESOURCE' = 1, 'SPAN' = 2`
 )
