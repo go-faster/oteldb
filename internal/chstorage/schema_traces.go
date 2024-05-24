5c059797f141b8177d81075a25b9c2eae30fe49a
tdakkota <tanc13@yandex.ru>
2024-05-24 10:58:57 +0300

    fix(chstorage): use proper enum DDL for tag scopes

--- internal/chstorage/schema_traces.go
+++ internal/chstorage/schema_traces.go
@@ -60,5 +60,5 @@ ORDER BY (service_namespace, service_name, resource, start)
 	ENGINE = ReplacingMergeTree
 	ORDER BY (value_type, name, value);`
 	valueTypeDDL = `'EMPTY' = 0,'STR' = 1,'INT' = 2,'DOUBLE' = 3,'BOOL' = 4,'MAP' = 5,'SLICE' = 6,'BYTES' = 7`
-	scopeTypeDDL = `'NONE' = 0, 'RESOURCE' = 1, 'SPAN' = 2`
+	scopeTypeDDL = `'NONE' = 0, 'RESOURCE' = 1, 'SPAN' = 2, 'INSTRUMENTATION' = 3`
 )
