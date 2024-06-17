ac16a14d05cc0d443065585dde52c5d8d5305bc4
tdakkota <tanc13@yandex.ru>
2024-06-17 05:22:30 +0300

    feat(otelbench): teach `promql analyze` to print reports in benchstat format

--- cmd/otelbench/promql_analyze.go
+++ cmd/otelbench/promql_analyze.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"os"
+	"slices"
 	"time"
 
 	"github.com/dustin/go-humanize"
 	"github.com/go-faster/errors"
 	yamlx "github.com/go-faster/yaml"
 	"github.com/spf13/cobra"
+	"golang.org/x/perf/benchfmt"
 )
 
 type PromQLAnalyze struct {
-	Input string
+	Input  string
+	Format string
 }
 
 func (a PromQLAnalyze) Run() error {
@@ -25,24 +30,39 @@ func (a PromQLAnalyze) Run() error {
 		return errors.Wrap(err, "unmarshal yaml")
 	}
 
+	if a.Format == "" {
+		a.Format = "pretty"
+	}
+	switch a.Format {
+	case "pretty":
+		return a.renderPretty(report, os.Stdout)
+	case "benchstat":
+		return a.renderBenchstat(report, os.Stdout)
+	default:
+		return errors.Errorf("unknown format %q", a.Format)
+	}
+}
+
+func (a PromQLAnalyze) renderPretty(report PromQLReport, w io.Writer) error {
+	var buf bytes.Buffer
 	for _, q := range report.Queries {
 		if q.ID != 0 {
-			fmt.Printf("[Q%d] ", q.ID)
+			fmt.Fprintf(&buf, "[Q%d] ", q.ID)
 		}
 		if q.Query != "" {
-			fmt.Println(q.Query)
+			fmt.Fprintln(&buf, q.Query)
 		} else {
-			fmt.Println(q.Matchers)
+			fmt.Fprintln(&buf, q.Matchers)
 		}
 
 		formatNanos := func(nanos int64) string {
 			d := time.Duration(nanos) * time.Nanosecond
 			return d.Round(time.Millisecond / 20).String()
 		}
-		fmt.Println(" duration:", formatNanos(q.DurationNanos))
+		fmt.Fprintln(&buf, " duration:", formatNanos(q.DurationNanos))
 
 		if len(q.Queries) > 0 {
-			fmt.Println(" sql queries:", len(q.Queries))
+			fmt.Fprintln(&buf, " sql queries:", len(q.Queries))
 
 			var memUsage, readBytes, readRows int64
 			for _, v := range q.Queries {
@@ -51,12 +71,70 @@ func (a PromQLAnalyze) Run() error {
 				readRows += v.ReadRows
 			}
 
-			fmt.Println(" memory usage:", humanize.Bytes(uint64(memUsage)))
-			fmt.Println(" read bytes:", humanize.Bytes(uint64(readBytes)))
-			fmt.Println(" read rows:", fmtInt(int(readRows)))
+			fmt.Fprintln(&buf, " memory usage:", humanize.Bytes(uint64(memUsage)))
+			fmt.Fprintln(&buf, " read bytes:", humanize.Bytes(uint64(readBytes)))
+			fmt.Fprintln(&buf, " read rows:", fmtInt(int(readRows)))
 		}
 	}
+	_, err := buf.WriteTo(w)
+	return err
+}
 
+func (a PromQLAnalyze) renderBenchstat(report PromQLReport, w io.Writer) error {
+	var recs []benchfmt.Result
+	for _, q := range report.Queries {
+		rec := benchfmt.Result{
+			Name: bytes.Join(
+				[][]byte{
+					[]byte(`PromQL`),
+					fmt.Appendf(nil, "Query%d", q.ID),
+				},
+				[]byte{'/'},
+			),
+		}
+		rec.Values = append(rec.Values, benchfmt.Value{
+			Value: float64(q.DurationNanos),
+			Unit:  "ns/op",
+		})
+
+		var chDurationNanos, memUsage, readBytes, readRows int64
+		for _, v := range q.Queries {
+			chDurationNanos += v.DurationNanos
+			memUsage += v.MemoryUsage
+			readBytes += v.ReadBytes
+			readRows += v.ReadRows
+		}
+		rec.Values = append(rec.Values,
+			benchfmt.Value{
+				Value: float64(chDurationNanos),
+				Unit:  "ch-ns/op",
+			},
+			benchfmt.Value{
+				Value: float64(memUsage),
+				Unit:  "ch-mem-usage/op",
+			},
+			benchfmt.Value{
+				Value: float64(readBytes),
+				Unit:  "ch-read-bytes/op",
+			},
+			benchfmt.Value{
+				Value: float64(readRows),
+				Unit:  "ch-read-rows/op",
+			},
+		)
+
+		recs = append(recs, rec)
+	}
+	slices.SortFunc(recs, func(a, b benchfmt.Result) int {
+		return bytes.Compare(a.Name, b.Name)
+	})
+
+	fw := benchfmt.NewWriter(w)
+	for i := range recs {
+		if err := fw.Write(&recs[i]); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -71,5 +149,6 @@ func newPromQLAnalyzeCommand() *cobra.Command {
 	}
 	f := cmd.Flags()
 	f.StringVarP(&p.Input, "input", "i", "report.yml", "Input file")
+	f.StringVarP(&p.Format, "format", "f", "pretty", "Output format")
 	return cmd
 }
--- go.mod
+++ go.mod
@@ -72,6 +72,7 @@ require (
 	go4.org/netipx v0.0.0-20230303233057-f1b76eb4bb35
 	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842
 	golang.org/x/oauth2 v0.21.0
+	golang.org/x/perf v0.0.0-20240604174448-3b48cf0e0164
 	golang.org/x/sync v0.7.0
 	golang.org/x/tools v0.22.0
 	google.golang.org/grpc v1.64.0
@@ -92,6 +93,7 @@ require (
 	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/Microsoft/hcsshim v0.11.4 // indirect
 	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/aclements/go-moremath v0.0.0-20210112150236-f10218a38794 // indirect
 	github.com/agext/levenshtein v1.2.1 // indirect
 	github.com/alecthomas/participle/v2 v2.1.1 // indirect
 	github.com/alecthomas/units v0.0.0-20231202071711-9a357b53e9c9 // indirect
