a2414c422190f87fd95b76964e3dc713277c89a7
tdakkota <tanc13@yandex.ru>
2024-06-17 06:55:03 +0300

    feat(ch-bench-read): add script to run benchmark locally

---
+++ dev/local/ch-bench-read/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+clean_up() {
+    ARG=$?
+    echo ">> Stopping"
+    docker compose down -v
+    exit $ARG
+}
+trap clean_up EXIT
+
+set -e
+
+echo ">> Setup oteldb locally"
+docker compose up -d --remove-orphans --build --force-recreate
+
+echo ">> Upload benchmark data"
+wget -N -O req.rwq https://storage.yandexcloud.net/faster-public/oteldb/req.rwq
+go run github.com/go-faster/oteldb/cmd/otelbench promrw replay -i req.rwq
+
+queries_file="${BENCH_QUERIES:-testdata/node-exporter-selected.promql.yml}"
+
+echo ">> Warmup"
+go run github.com/go-faster/oteldb/cmd/otelbench promql bench \
+    -i "$queries_file" \
+    --warmup 10
+
+echo ">> Benchmark"
+OTEL_EXPORTER_OTLP_INSECURE="true" go run github.com/go-faster/oteldb/cmd/otelbench promql bench \
+    -i "$queries_file" \
+    -o report.yml \
+    --trace \
+    --allow-empty=false \
+    --count 5 \
+    --warmup 5
+
+echo ">> Done"
