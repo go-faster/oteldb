cef5f8f9a719d74e6d08a5e126b11931c4f6e6db
tdakkota <tanc13@yandex.ru>
2024-06-20 10:49:04 +0300

    feat(chsql): add `hasToken` and `like` functions

--- internal/chstorage/chsql/function.go
+++ internal/chstorage/chsql/function.go
@@ -70,6 +70,16 @@ func Length(arg Expr) Expr {
 	return Function("length", arg)
 }
 
+// Like returns `like(<haystack>, <pattern>)` function call expression.
+func Like(haystack, pattern Expr) Expr {
+	return Function("like", haystack, pattern)
+}
+
+// Position returns `position(<haystack>, <needle>)` function call expression.
+func Position(haystack, needle Expr) Expr {
+	return Function("position", haystack, needle)
+}
+
 // PositionUTF8 returns `positionUTF8(<haystack>, <needle>)` function call expression.
 func PositionUTF8(haystack, needle Expr) Expr {
 	return Function("positionUTF8", haystack, needle)
@@ -78,6 +88,11 @@ func PositionUTF8(haystack, needle Expr) Expr {
 // Match returns `match(<haystack>, <pattern>)` function call expression.
 func Match(haystack, pattern Expr) Expr {
 	return Function("match", haystack, pattern)
+}
+
+// HasToken returns `hasToken(<haystack>, <token>)` function call expression.
+func HasToken(haystack Expr, token string) Expr {
+	return Function("hasToken", haystack, String(token))
 }
 
 // JSONExtract returns `JSONExtract(<from>, <typ>)` function call expression.
