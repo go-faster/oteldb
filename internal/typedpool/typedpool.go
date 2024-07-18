f5652b1795a6334ad1332b93c4fb23e775cba0e4
tdakkota <tanc13@yandex.ru>
2024-07-18 11:52:45 +0300

    feat(typedpool): add typed `sync.Pool` wrapper

---
+++ internal/typedpool/typedpool.go
@@ -0,0 +1,43 @@
+// Package typepool contains generic wrapper around [sync.Pool].
+package typedpool
+
+import "sync"
+
+// Pool is a typed wrapper around [sync.Pool].
+type Pool[P *T, T any] struct {
+	pool sync.Pool
+}
+
+func NewPool[P *T, T any](newFunc func() P) *Pool[P, T] {
+	return &Pool[P, T]{
+		pool: sync.Pool{
+			New: func() any {
+				return newFunc()
+			},
+		},
+	}
+}
+
+// Put returns P to pool.
+func (p *Pool[P, T]) Put(item P) {
+	p.pool.Put(item)
+}
+
+// Get returns a P from pool.
+func (p *Pool[P, T]) Get() P {
+	return p.pool.Get().(P)
+}
+
+// GetReset gets a P from pool and resets it.
+func GetReset[
+	P interface {
+		// Reset resets item state for re-use.
+		Reset()
+		*T
+	},
+	T any,
+](p *Pool[P, T]) P {
+	item := p.Get()
+	item.Reset()
+	return item
+}
