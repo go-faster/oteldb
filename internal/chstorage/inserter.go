3e7e0c4d3755f47ac0cfe74a350176092c94ba95
Aleksandr Razumov <ernado@go-faster.org>
2025-11-01 22:28:13 +0300

    feat(chstorage): add totals

--- internal/chstorage/inserter.go
+++ internal/chstorage/inserter.go
@@ -1,9 +1,12 @@
 package chstorage
 
 import (
+	"context"
+
 	"github.com/go-faster/errors"
 	"github.com/go-faster/sdk/autometric"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/trace"
 
@@ -75,6 +78,67 @@ func NewInserter(c ClickHouseClient, opts InserterOptions) (*Inserter, error) {
 		Prefix: "chstorage.",
 	}); err != nil {
 		return nil, errors.Wrap(err, "init stats")
+	}
+
+	totalLogs, err := meter.Int64ObservableGauge("chstorage.logs.inserted_records_total",
+		metric.WithDescription("Total number of inserted log records"),
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "create total inserted log records counter")
+	}
+	totalMetrics, err := meter.Int64ObservableGauge("chstorage.metrics.inserted_points_total",
+		metric.WithDescription("Total number of inserted metric points"),
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "create total inserted metric points counter")
+	}
+	totalTraces, err := meter.Int64ObservableGauge("chstorage.traces.inserted_spans_total",
+		metric.WithDescription("Total number of inserted spans"),
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "create total inserted spans counter")
+	}
+
+	_, err = meter.RegisterCallback(func(ctx context.Context, o metric.Observer) error {
+		type total struct {
+			Table    string
+			Signal   string
+			Observed metric.Int64Observable
+		}
+		for _, t := range []total{
+			{
+				Table:    inserter.tables.Logs,
+				Signal:   "logs",
+				Observed: totalLogs,
+			},
+			{
+				Table:    inserter.tables.Points,
+				Signal:   "metrics",
+				Observed: totalMetrics,
+			},
+			{
+				Table:    inserter.tables.Spans,
+				Signal:   "traces",
+				Observed: totalTraces,
+			},
+		} {
+			v, err := inserter.totals(ctx, t.Table)
+			if err != nil {
+				return errors.Wrapf(err, "get totals for table %q", t.Table)
+			}
+			o.ObserveInt64(t.Observed, v, metric.WithAttributes(
+				attribute.String("chstorage.signal", t.Signal),
+			))
+		}
+
+		return nil
+	},
+		totalLogs,
+		totalMetrics,
+		totalTraces,
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "register totals callback")
 	}
 
 	return inserter, nil
