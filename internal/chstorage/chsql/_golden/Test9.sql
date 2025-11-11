4fbe93ff1edc552257f3b369828721eb9bf16b4f
tdakkota <tdakkota@yandex.ru>
2025-11-12 01:22:10 +0300

    chore: commit generated files

--- internal/chstorage/chsql/_golden/Test9.sql
+++ internal/chstorage/chsql/_golden/Test9.sql
@@ -1,1 +1,1 @@
-SELECT body FROM logs GROUP BY body,timestamp HAVING hasToken(body, 'Error'),hasToken(level, 'Error')
+SELECT body FROM logs GROUP BY body,timestamp HAVING hasToken(body, 'Error') AND hasToken(level, 'Error')
