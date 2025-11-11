be220fa0257ab85fc42e80c061db46432866c75b
tdakkota <tdakkota@yandex.ru>
2025-11-11 13:39:02 +0300

    chore: commit generated files

--- internal/chstorage/_golden/schema.metrics_exemplars.sql
+++ internal/chstorage/_golden/schema.metrics_exemplars.sql
@@ -1,16 +1,13 @@
 CREATE TABLE IF NOT EXISTS `metrics_exemplars`
 (
-	`name`                LowCardinality(String),
-	`timestamp`           DateTime64(9)          CODEC(Delta, ZSTD(1)),
+	`hash`                FixedString(16),
+	`timestamp`           DateTime64(9)   CODEC(Delta, ZSTD(1)),
 	`filtered_attributes` String,
-	`exemplar_timestamp`  DateTime64(9)          CODEC(Delta, ZSTD(1)),
+	`exemplar_timestamp`  DateTime64(9)   CODEC(Delta, ZSTD(1)),
 	`value`               Float64,
 	`trace_id`            FixedString(16),
-	`span_id`             FixedString(8),
-	`attribute`           LowCardinality(String),
-	`resource`            LowCardinality(String),
-	`scope`               LowCardinality(String)
+	`span_id`             FixedString(8)
 )
 ENGINE = MergeTree
 PARTITION BY toYYYYMMDD(timestamp)
-ORDER BY (`name`, `resource`, `attribute`, `timestamp`)
+ORDER BY (`hash`, `timestamp`)
