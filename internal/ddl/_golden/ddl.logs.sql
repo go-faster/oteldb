364b02065e5d714e15ae816f5d732f404aa09b5d
tdakkota <tdakkota@yandex.ru>
2025-11-14 00:02:46 +0300

    chore: commit generated files

--- internal/ddl/_golden/ddl.logs.sql
+++ internal/ddl/_golden/ddl.logs.sql
@@ -10,5 +10,5 @@ CREATE TABLE IF NOT EXISTS `logs` ON CLUSTER `foo`
 	INDEX `attribute_keys` arrayConcat(JSONExtractKeys(attribute), JSONExtractKeys(scope), JSONExtractKeys(resource)) TYPE set(100)
 )
 ENGINE = MergeTree()
-ORDER BY (`a`, `b`)
+ORDER BY (`a`, toStartOfHour(b))
 TTL toDateTime(`timestamp`) + toIntervalSecond(36000)
